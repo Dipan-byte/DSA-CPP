3728726a776413c262d29636dff825fb7011ad8a
Dipan Mallick <dipanmallick39@gmail.com>
2025-12-16 23:13:44 +0530

    done

---
+++ maxsubarr.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int arr[n];
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    int k;
+    cin >> k;
+
+    int windowSum = 0;
+    for (int i = 0; i < k; i++)
+        windowSum += arr[i];
+
+    int maxSum = windowSum;
+
+    for (int i = k; i < n; i++) {
+        windowSum += arr[i] - arr[i - k];
+        maxSum = max(maxSum, windowSum);
+    }
+
+    cout << "Maximum Sum: " << maxSum;
+    return 0;
+}
