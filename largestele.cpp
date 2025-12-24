c6ed4f30dfe9c1c36eb5bea9b9e29cb000a41d38
Dipan Mallick <dipanmallick39@gmail.com>
2025-12-24 21:13:46 +0530

    done

---
+++ largestele.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[] = {10, 45, 23, 89, 12};
+    int n = 5;
+    int max = arr[0];
+
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+
+    cout << "Largest element = " << max;
+    return 0;
+}
